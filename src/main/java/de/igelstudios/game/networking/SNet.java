c530f147bd571fae33bef447f9fe266f8369f008
EinePerson <einenperson@gmail.com>
2023-06-29 14:37:10 +0200

    Changed Networking

---
+++ src/main/java/de/igelstudios/game/networking/SNet.java
@@ -0,0 +1,10 @@
+package de.igelstudios.game.networking;
+
+import de.igelstudios.igelengine.common.networking.server.Server;
+
+public class SNet {
+
+    public static void register(){
+        Server.registerClient2ServerHandler("Login",C2SLogIn::receive);
+    }
+}
